3673745bdde84cf2de48d1ecd4cd661a7c2652e3
John Thompson <john@iniveo.co.uk>
2020-02-21 14:55:31 +0000

    Removed all delays - fixed downlink handling - update docs
    This is a working measurement system

---
+++ Arduino/README.md
@@ -0,0 +1,39 @@
+# Arduino ORP demo
+This Arduino application reads temperature from a thermistor and sends the temperature value to the Octave cloud.  
+
+## Hardware
+This is a list of the hardware used to test the application.  
+
+* Arduino Mega 2560
+* mangOH yellow WP7702 with Octave build Version 2.1.0 (octave_2.1.3_wp77xx Legato version 19.07 Modem firmware version R12)
+* SEEED Grove - Temperature Sensor V1.2 attached to Arduino ADC 0
+
+## Useful websites
+For the cloud data  
+https://octave.sierrawireless.io/  
+Login to the account where the particular mangOH yellow has been registered  
+
+For the temperature sensor  
+http://wiki.seeedstudio.com/Grove-Temperature_Sensor_V1.2/  
+
+For the Arduino mega 2560  
+https://www.arduino.cc/en/Guide/ArduinoMega2560  
+
+For the mangOH yellow  
+https://mangoh.io/mangoh-yellow  
+
+
+
+## Arduino software
+The Sketch is called sketch_feb09a.ino  
+
+The git repo also includes libraries that are needed to build the application  
+* ArduOrp
+* SimpleTimer-1.0.0
+
+## Github
+
+Current Github location is 
+
+
+
--- Arduino/octaveArduino/sketch_feb09a/sketch_feb09a.ino
+++ Arduino/octaveArduino/sketch_feb09a/sketch_feb09a.ino
@@ -1,19 +1,31 @@
-/*
- * Working a bit better but I'm having some issues with my level shifter
- * Hence the commented out frame waiting code
+/* 
+ *  For spec on ORP
+ *  https://docs.octave.dev/references/edge/octave_resource_protocol/#orp-api
  */
 
 
+
+#include <avr/pgmspace.h>
+#include <math.h>
+
 #include <Arduhdlc.h>
-
 #include <orp.h>
+#include <SimpleTimer.h>  // polled timer slightly better than delay()
 
-#define MAX_HDLC_FRAME_LENGTH 64
+#define TIME_CALL_MS 1000
+SimpleTimer firstTimer(TIME_CALL_MS);
+
+SimpleTimer secondTimer((uint64_t)TIME_CALL_MS * (uint64_t)120); // measurement data orp rate
+
+#define MAX_HDLC_FRAME_LENGTH   128
+#define MAX_ORP_RX_FRAME_LENGTH 128
+#define MAX_ORP_TX_FRAME_LENGTH 128
+
 /* Function to send out byte/char */
-void send_character(uint8_t data);
+static void send_character(uint8_t data);
 
 /* Function to handle a valid HDLC frame */
-void hdlc_frame_handler(const uint8_t *data, uint16_t length);
+static void hdlc_frame_handler(const uint8_t *data, uint16_t length);
 
 /* Initialize Arduhdlc library with three parameters.
 1. Character send function, to send out HDLC frame one byte at a time.
@@ -21,40 +33,44 @@ void hdlc_frame_handler(const uint8_t *data, uint16_t length);
 3. Length of the longest frame used, to allocate buffer in memory */
 Arduhdlc hdlc(&send_character, &hdlc_frame_handler, MAX_HDLC_FRAME_LENGTH);
 
-char payload[128] = "hello";
+static char orp_responseFrame[MAX_ORP_RX_FRAME_LENGTH] = ""; 
+static char orp_txPayload[MAX_ORP_TX_FRAME_LENGTH] = "";
 
-orp orpO(payload, sizeof(payload));
+orp orpO(orp_txPayload, sizeof(orp_txPayload));
 
 /* Function to send out one 8bit character */
-void send_character(uint8_t data) {
+static void send_character(uint8_t data) {
     Serial2.print((char)data);
 }
 
-// quick bodge to handle response frame
-bool respFrame = false;
+
+
 
 /* Frame handler function. What to do with received data? */
-void hdlc_frame_handler(const uint8_t *data, uint16_t length) {
+static void hdlc_frame_handler(const uint8_t *data, uint16_t length) {
     // Do something with data that is in framebuffer
     Serial.println("Frame");
+
+    memcpy(orp_responseFrame, data , sizeof(orp_responseFrame));
+    
     for(uint16_t i = 0; i < length; i++)
     {
-        Serial.print((char)data[i]);
+        Serial.print((char)orp_responseFrame[i]);
         Serial.print("[");
-        Serial.print(data[i],HEX);
+        Serial.print(orp_responseFrame[i],HEX);
         Serial.print("] ");
     }  
     Serial.println(" ");
-    respFrame = true;
+    
 }
 
 
-void debugPayload(void)
+static void debugPayload(void)
 {
-  Serial.print("Payload "); Serial.print(payload); Serial.print(" Len ");Serial.println(strlen(payload));
+  Serial.print("Payload "); Serial.print(orp_txPayload); Serial.print(" Len ");Serial.println(strlen(orp_txPayload));
 }
 
-void wakeup(void)
+static void wakeup(void)
 {
     send_character('~');
     delay(100);
@@ -63,47 +79,45 @@ void wakeup(void)
     send_character('~');
 }
 
-void setup() { 
-    pinMode(16, OUTPUT); // Serial port TX to output
-    pinMode(17, INPUT);
-    digitalWrite(17, LOW); // disable internal pullup
-    
-    // initialize serial port to 9600 baud
-    Serial2.begin(9600);
-    Serial.begin(115200);
-    Serial.println("Init");
-    // checkSend();
-    
 
+static void octave_registerInput(void)
+{
     int16_t retVal;
-    // while(respFrame == false)
-    {
-      retVal = orpO.createResource(SBR_PKT_RQST_INPUT_CREATE, SBR_DATA_TYPE_NUMERIC, "val/te", "num"  );
-      Serial.println(retVal);
-      debugPayload();
-      wakeup(); 
-      hdlc.frameEncode((uint8_t *) payload, strlen(payload));
-      delay(500);
-    }
-    respFrame = false;
-  
-    Serial.println("Init done");
+    retVal = orpO.createResource(SBR_PKT_RQST_INPUT_CREATE, SBR_DATA_TYPE_NUMERIC, "val/te", "num"  );
+    Serial.print("orp encode "); Serial.println(retVal);
+    debugPayload();
+    wakeup(); 
+    hdlc.frameEncode((uint8_t *) orp_txPayload, strlen(orp_txPayload));
 }
 
 
-void loop() {
-    delay(10000);
-    // while(respFrame == false)
-    {
-      orpO.pushValue(SBR_DATA_TYPE_NUMERIC, "val/te" , "25.2" );
-      debugPayload();
-      wakeup();
-      hdlc.frameEncode((uint8_t *) payload, strlen(payload));
-      delay(2000);
-    }
-    respFrame = false;
-}
 
+// read the sensor and send the data to Octave
+static void octave_sendInput(void)
+{
+    int16_t retVal;
+    
+    // read the sensor
+    static int a;
+    static float temperature;
+    static int B=3975;    //B value of the thermistor
+    static float resistance;
+    static char outstr[15];
+    
+    a=analogRead(0);                                         // I used http://wiki.seeedstudio.com/Grove-Temperature_Sensor/
+    resistance=(float)(1023-a)*10000/a;                      // get the resistance of the sensor;
+    temperature=1/(log(resistance/10000)/B+1/298.15)-273.15; // convert to temperature via datasheet;;
+    Serial.print("Current temperature is ");
+    dtostrf(temperature,1, 1, outstr);
+    Serial.println(outstr);
+
+    // send the sensor data to octave via orp
+    retVal = orpO.pushValue(SBR_DATA_TYPE_NUMERIC, "val/te" , outstr );
+    Serial.print("orp encode "); Serial.println(retVal);
+    debugPayload();
+    wakeup();
+    hdlc.frameEncode((uint8_t *) orp_txPayload, strlen(orp_txPayload));
+}
 
 
 /*
@@ -111,21 +125,66 @@ SerialEvent occurs whenever a new data comes in the
 hardware serial RX.  This routine is run between each
 time loop() runs, so using delay inside loop can delay
 response.  Multiple bytes of data may be available.
+Damn I hadn't realised that this won't run if stuck in loop
 */
 void serialEvent2() {
-    //Serial.println("Sta");
-    while (Serial2.available()) {
+    while (Serial2.available()) 
+    {
         // get the new byte:
         int inChar = Serial2.read();
         // Pass all incoming data to hdlc char receiver
-        //Serial.print((char)inChar);
-        //Serial.print(" ");
-        //Serial.print(inChar,HEX);
-        //Serial.print(", ");
         hdlc.frameDecode_char((uint8_t )inChar);
     }
-    //Serial.println("End");
-    // Serial.print(" [" );
-    //Serial.print(hdlc.debug() );
-    // Serial.print("] " );
+}
+
+
+/*
+ * standard arduino stuff
+ */
+
+void setup() { 
+  pinMode(16, OUTPUT); // Serial port TX to output
+  pinMode(17, INPUT);
+  digitalWrite(17, LOW); // disable internal pullup
+  
+  // initialize serial port to 9600 baud
+  Serial2.begin(9600);
+  Serial.begin(115200);
+  Serial.println("Init");
+
+  Serial.println("Init done");
+}
+
+// core state machine
+void loop() 
+{
+  static uint8_t state = 0;
+  
+  if(orp_responseFrame[0] == 'i')   // very basic response check - needs work
+  {
+    state = 1;
+    orp_responseFrame[0] = '\0';    // clear frame data 
+    octave_sendInput(); // try and send first data set
+  }
+
+  if (firstTimer.isReady())
+  {
+    firstTimer.reset();
+    
+    switch(state)
+    {
+      case 0:
+        octave_registerInput();
+        break;
+      case 1:
+        if(secondTimer.isReady())
+        {
+          secondTimer.reset();
+          octave_sendInput();
+        }
+        break;
+      default:
+        state = 0;  
+    }
+  }
 }
