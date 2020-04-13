f9df5c159e1396d223e3d8a04b1b6c08f3448aa8
John Thompson <john@iniveo.co.uk>
2020-04-13 16:46:17 +0100

    cleanup

--- c/atmel/atmel/Debug/main.d
+++
@@ -1,7 +0,0 @@
-main.d main.o: .././main.c \
- f:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- f:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-f:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-f:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
